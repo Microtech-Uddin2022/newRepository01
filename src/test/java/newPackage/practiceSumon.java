8aed6698215880143b1798b7d1ff08f114a47b6c
smrafiq <145926113+smrafiq@users.noreply.github.com>
2023-10-09 23:38:23 -0400

    I delete a line in the class

--- src/test/java/newPackage/practiceSumon.java
+++ src/test/java/newPackage/practiceSumon.java
@@ -3,6 +3,6 @@ package newPackage;
 public class practiceSumon {
     public static void main(String[] args) {
         System.out.println("hello team");
-        System.out.println("created a new branch");
+      
     }
 }
