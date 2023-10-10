53a20b45a280a60f257328abf1daee2d8728f2c6
micr0tect@NA <joejesper22@gmail.com>
2023-10-09 23:34:00 -0400

    checking merge conflict

--- src/test/java/newPackage/practiceSumon.java
+++ src/test/java/newPackage/practiceSumon.java
@@ -4,7 +4,7 @@ public class practiceSumon {
     public static void main(String[] args) {
         System.out.println("hello team");
         System.out.println("created a new branch");
-        System.out.println("check merge conflict issue");
+        System.out.println("check merge conflict issue1");
 
     }
 }
