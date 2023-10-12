294ddf44f6275a1b26315ecce8bccf4a43c1353b
QuaziHoque <quazihoque16@gmail.com>
2023-10-12 15:24:37 -0500

    REV

--- src/test/java/NewGitHubMethod.java
+++ src/test/java/NewGitHubMethod.java
@@ -3,8 +3,13 @@ public class NewGitHubMethod {
     public static void main(String[] args) {
         System.out.println("We are creating new line");
         System.out.println("We also added another line");
-        System.out.println(" First line added");
-        System.out.println(" Second line added");
-        System.out.println(" Third line added");
+
+
+        System.out.println("We are creating  1 line");
+        System.out.println("We also added another 2 line");
+        System.out.println("We are creating new 3 line");
+        System.out.println("We also added another 4line");
+
+
     }
 }
