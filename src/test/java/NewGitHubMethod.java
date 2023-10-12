33b8b6859f18b996802f8513439b432c137affb4
NeromKhan <145925862+NeromKhan@users.noreply.github.com>
2023-10-12 16:50:02 -0400

    Update NewGitHubMethod.java

--- src/test/java/NewGitHubMethod.java
+++ src/test/java/NewGitHubMethod.java
@@ -3,12 +3,5 @@ public class NewGitHubMethod {
     public static void main(String[] args) {
         System.out.println("We are creating new line");
         System.out.println("We also added another line");
-
-
-
-        System.out.println(" Third line added");
-        System.out.println("4th Line added");
-        System.out.println("We are creating 10 line");
-        System.out.println("We also added another 11 line");
     }
 }
