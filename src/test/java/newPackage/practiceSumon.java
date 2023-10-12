a6dd6378fee9bbb9530523a412f1dcb6c1a8a6c0
NeromKhan <145925862+NeromKhan@users.noreply.github.com>
2023-10-12 16:51:58 -0400

    Update practiceSumon.java

--- src/test/java/newPackage/practiceSumon.java
+++ src/test/java/newPackage/practiceSumon.java
@@ -3,6 +3,7 @@ package newPackage;
 public class practiceSumon {
     public static void main(String[] args) {
         System.out.println("hello team");
+        System.out.println("nerom added new line")
       
     }
 }
